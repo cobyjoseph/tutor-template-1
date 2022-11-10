894bab0ffd1533b96d74804d3de7242854bf79f6
Coby Joseph <coby.joseph@gmail.com>
2022-11-09 19:11:04 -0500

    main text

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,9 +1,11 @@
-<div class=" relative flex justify-between text-2xl mt-8">
-	<div class=" relative text-secondary ml-20">Company Name / Logo</div>
-	<div class=" relative flex bg-red-200  gap-14 mr-20  ">
-		<div class="text-primary  ">Who we are</div>
-		<div class="text-primary ">Our services</div>
-		<div class="text-primary ">Pricing</div>
-		<div class="text-primary ">Contact us</div>
+<div
+	class=" relative flex justify-between text-2xl  bg-third bg-opacity-80 rounded-lg py-1 px-14 text-white"
+>
+	<div class=" relative  ">Company Name / Logo</div>
+	<div class=" relative flex   gap-14   ">
+		<div class=" font-medium  ">Who we are</div>
+		<div class=" font-medium  ">Our services</div>
+		<div class=" font-medium ">Pricing</div>
+		<div class=" font-medium ">Contact us</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
 <script>
 	import Menu from '../lib/components/Menu.svelte';
 </script>
+
+<div class="absolute transform top-1/2 -translate-y-1/2 ml-16">
+	<div class="font-extrabold text-transparent text-5xl bg-clip-text gradient">
+		<div>Build confidence.</div>
+		<div>Get results.</div>
+	</div>
+</div>
+
+<style>
+	.gradient {
+		background-image: linear-gradient(120deg, #3709ee, #0284da 35% 100%);
+	}
+</style>
