b352552a70e3a7f9aef18d1cfc178bf174041be7
Coby Joseph <coby.joseph@gmail.com>
2022-11-15 10:40:40 -0500

    added svgs

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,7 +1,23 @@
 <div
 	class=" relative flex justify-between text-xl  bg-opacity-20 rounded-lg py-1 px-14 text-primary mt-6 overflow-hidden"
 >
-	<div class=" relative  ">Company Name / Logo</div>
+	<div class="flex gap-2">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="2"
+			stroke="currentColor"
+			class="w-8 h-8"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+			/>
+		</svg>
+		<div class=" relative font-semibold text-2xl  ">Company Name</div>
+	</div>
 	<div class=" relative flex   gap-14   ">
 		<div class=" font-medium  ">Who we are</div>
 		<div class=" font-medium  ">Our services</div>
