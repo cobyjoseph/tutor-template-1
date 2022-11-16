6fa9a1ea387808f592632974e435a03e7244115a
Coby Joseph <coby.joseph@gmail.com>
2022-11-16 12:23:38 -0500

    css changes

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,27 +1,29 @@
-<div
-	class=" font-Heebo  relative flex justify-between text-xl bg-white bg-opacity-20 rounded-lg px-14 text-primary py-2  overflow-hidden"
->
-	<div class="flex gap-2">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="2"
-			stroke="currentColor"
-			class="w-8 h-8"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-			/>
-		</svg>
-		<div class=" relative font-semibold text-2xl  ">Company Name</div>
-	</div>
-	<div class=" relative flex   gap-14   ">
-		<div class=" font-medium  ">Who we are</div>
-		<div class=" font-medium  ">Our services</div>
-		<div class=" font-medium ">Pricing</div>
-		<div class=" font-medium ">Contact us</div>
+<div class="bg-white bg-opacity-40 flex justify-center">
+	<div
+		class=" font-Heebo  w-full max-w-[1400px] relative flex justify-between text-xl rounded-lg px-[3%] text-primary py-2  overflow-hidden"
+	>
+		<div class="flex gap-2">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				class="w-8 h-8"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+				/>
+			</svg>
+			<div class=" relative font-semibold text-2xl  ">Company Name</div>
+		</div>
+		<div class=" relative flex   gap-14   ">
+			<div class=" font-medium  ">Who we are</div>
+			<div class=" font-medium  ">Our services</div>
+			<div class=" font-medium ">Pricing</div>
+			<div class=" font-medium ">Contact us</div>
+		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,30 +6,37 @@
 
 <Background />
 
-<div class="relative mx-4 mb-6 grid h-full grid-cols-1 max-w-[1200px] ml-auto mr-auto bg-green-400">
-	<div class="relative transform top-1/2 -translate-y-1/2  ml-16 overflow-hidden z-10">
-		<div class="font-extrabold text-transparent text-5xl bg-clip-text gradient">
-			<div>Build confidence.</div>
-			<div>Get results.</div>
-		</div>
-		<div class="mt-3  text-xl text-[#544E4E]">Tutoring for a wide range of subjects and ages.</div>
+<!-- TODO  - ON LARGE SCREEN MAKE EVERYTHING CLOSER TO THE TOP BY INCREASING THE NEGATIVE MARGIN TOP OR SOMETHING -->
 
-		<div class="flex items-end ">
-			<div
-				class=" rounded-full mt-4 inline-block bg-primary px-7 text-xl text-white font-semibold py-2 "
-			>
-				See services
+<div class="-mt-16 flex justify-center items-center h-[100vh]">
+	<div class="px-[3%] grid grid-cols-1 max-w-[1400px] z-10">
+		<div class="grid grid-cols-2  ">
+			<!-- LEFT COLUMN -->
+			<div class="flex items-center">
+				<div class=" ">
+					<div class="text-5xl font-extrabold text-transparent  bg-clip-text gradient">
+						<div>Build confidence.</div>
+						<div>Get results.</div>
+					</div>
+					<div class="mt-3  text-xl text-[#544E4E]">
+						Tutoring for a wide range of subjects and ages.
+					</div>
+					<div
+						class=" rounded-full mt-4 inline-block bg-primary px-7 text-xl text-white font-semibold py-2 "
+					>
+						See services
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
+			<!-- LEFT COLUMN END -->
 
-	<!-- PHOTO OF STUDENT -->
-	<div class="">
-		<div class="absolute transform top-1/2 -translate-y-1/2 right-12 max-w-xl z-10 ">
 			<img src="tutor-graphic-1.svg" alt="Student working at computer" />
 		</div>
 	</div>
 </div>
-<!-- END OF PHOTO OF STUDENT -->
 
-<!-- Photo from https://www.flickr.com/photos/83633410@N07/7658219802/  from CollegeDegrees360, Flickr 2012, background edited -->
+<style>
+	.gradient {
+		background-image: linear-gradient(120deg, #3709ee, #0284da 35% 100%);
+	}
+</style>
