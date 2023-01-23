72d4849eaece74abc007a950a811268a30e24985
Coby Joseph <coby.joseph@gmail.com>
2023-01-23 14:12:10 -0300

    does this upload?

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,12 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: '#3709EE',
-				secondary: '#C51616',
+				primary: '#aad3ff',
+				primaryDark: '#56A6F9',
+				primaryBright: '#3116EC',
+				secondary: '#B0ABFF',
 				third: '#3988EC',
-				titles: '#2B217A',
+				textAlternate: '#2B217A',
 				background1: '#CBC8F0',
 				background2: '#DFE0FF',
 				background3: '#CAD3FF'
